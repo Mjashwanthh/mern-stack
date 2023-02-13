a0b44e034cbe8be96a86c9ab685021b4016e84db
Jashwanth <jashwanthmallela537@gmail.com>
2023-02-13 22:28:27 +0530

    added html file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("jashwanth");
+	res.sendFile(__dirname,"/index.html");
 });
 
 app.listen(port, function(){
