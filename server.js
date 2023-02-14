6a28705e0295c5898fa9e183efbf88ea6ea8eb46
Mjashwanthh <110723233+Mjashwanthh@users.noreply.github.com>
2023-02-14 20:30:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5010;
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html")
 });
+app.get("/resume", function(req, res){
+	res.sendFile(__dirname+"/resume.html")
+});
 
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
