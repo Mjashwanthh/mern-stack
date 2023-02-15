7d5c97ee633c9abf0018e7bbb6b06fb6b2318828
Jashwanth <jashwanthmallela537@gmail.com>
2023-02-15 21:10:03 +0530

    changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
 
-app.use(express.static(path.join(__dirname+'public')));
+app.use(express.static(path.join(__dirname+'/public')));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html")
