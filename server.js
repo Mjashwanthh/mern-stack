43909b8ff04076b4d8996feebe5c631173ef2556
Jashwanth <jashwanthmallela537@gmail.com>
2023-02-15 22:03:18 +0530

    database connected

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
 
-app.use(express.static(__dirname+'/public'));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html")
@@ -11,8 +13,17 @@ app.get("/", function(req, res){
 app.get("/resume", function(req, res){
 	res.sendFile(__dirname+"/resume.html")
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err) {
+		console.error(err);
+	}
+	else {
+		console.log("DB connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	}
 });
+
