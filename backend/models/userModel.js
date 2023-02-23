5fed5377f8fadfd3ded4715c940c96ac5e144c14
Jashwanth <jashwanthmallela235@gmail.com>
2023-02-23 21:51:21 +0530

    changed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,5 +7,5 @@ const userSchema = new mongoose.Schema({
     isDeleted : { type:Boolean , default : false }
 });
 
-module.exports = mongoose.model("user",userSchema);
-//export const userModel = mongoose.model("user",userSchema);
+//module.exports = mongoose.model("user",userSchema);
+export const userModel = mongoose.model("user",userSchema);
